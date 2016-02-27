38bf0690fdf1441953f26b0d0f93fd8a2897cad0
Slater Victoroff <slater@Slaters-MacBook-Pro.local>
2016-02-27 18:01:01 -0500

    acceptable temperature checking now works

--- Climate/ashrae.py
+++ Climate/ashrae.py
@@ -1,6 +1,9 @@
-from constants import wetbulb_b, wetbulb_c
+from constants import wetbulb_b, wetbulb_c, acceptable_temps
 
 import numpy as np
+import matplotlib.path as mplPath
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 def calculate_dew_point_temperature(temp, humidity):
 	"""
@@ -13,8 +16,19 @@ def calculate_dew_point_temperature(temp, humidity):
 	internal_formula = np.log(humidity) + ((wetbulb_b * temp) / (wetbulb_c + temp))
 	return (wetbulb_c * internal_formula) / (wetbulb_b - internal_formula)
 
+def check_acceptability(temp, humidity):
+	"""
+	Given a temperature and humidity, determine if the conditions are ASHRAE compliant
+	"""
+	acceptable_range = mplPath.Path(acceptable_temps)
+	dew_point = calculate_dew_point_temperature(temp, humidity)
+	print dew_point
+	return acceptable_range.contains_point((temp, dew_point))
+
 if __name__ == "__main__":
 	temp_vector = np.array([-20, -5, 10, 25])
 	humidity_vector = np.array([0.2, 0.5, 0.8, 0.3])
 	golden_vector = np.array([-37.26, -13.83, 6.71, 6.21])
-	print calculate_dew_point_temperature(temp_vector, humidity_vector) 
+	print calculate_dew_point_temperature(temp_vector, humidity_vector)
+
+	print check_acceptability(21, 0.4)
--- Climate/constants.py
+++ Climate/constants.py
@@ -1,2 +1,12 @@
+import numpy as np
+
 wetbulb_b = 17.62  # unitless
 wetbulb_c = 243.12  # degrees C
+
+# four pairs of operative temp, dew point temp in C
+acceptable_temps = np.array([
+	[20.28, 2.78],
+	[20, 11.67],
+	[25.83, 17.22],
+	[27.22, 2.78],
+])
